cfd7562a3fd8392398d8f233722785d9a4327551
WindeusK <129594654+WindeusK@users.noreply.github.com>
2023-08-30 20:23:41 +0700

    login function done

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,13 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     public function getLoginPage () {
         return view('login');
+    }
+
+    /**
+     * Function for login.
+     * Take credentials & then generate session with Laravel
+     */
+    public function login (Request $request) {
+        $credentials = $request->validate([
+            'email' => ['required','email'],
+            'password' => 'required'
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            return back()->withErrors(['email' => 'user not found']);
+        }
+
+        $request->session()->regenerate();
+        
+        return redirect()->intended('dashboard');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/login', [LoginController::class, 'getLoginPage']);
+Route::post('/login', [LoginController::class, 'login']);
