8b2fc521e313af012034b65f21fdf2fa6f03a8ad
WindeusK <129594654+WindeusK@users.noreply.github.com>
2023-08-31 13:34:54 +0700

    implement authorization guru/siswa

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    //
+    public function dashboard () {
+        $user = Auth::user();
+        if (Gate::allows('admin', $user)) {
+            return view('dashboard.guru', ['user' => $user]);
+        } else {
+            return view('dashboard.siswa', ['user' => $user]);
+        }
+    }
 }
