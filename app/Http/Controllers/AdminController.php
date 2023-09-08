6cb06eac9ef6e7a7e31e8aca77efe4c44c4cc55d
WindeusK <129594654+WindeusK@users.noreply.github.com>
2023-09-08 08:01:45 +0700

    admin dashboard function done

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Buku;
+use App\Models\Peminjaman;
 
 class DashboardController extends Controller
 {
     public function dashboard () {
         return view('admin.dashboard', [
-            'admin_info' => User::find(Auth::id())
+            'admin_info' => User::find(Auth::id()),
+            'preview_buku' => Buku::take(10)->get(),
+            'preview_peminjaman' => Peminjaman::take()->get()
         ]);
     }
 }
